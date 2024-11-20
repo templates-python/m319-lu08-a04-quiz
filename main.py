997dbb7bbb37b0e11e3c5f6f1ff5e01c2eb87659
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-11-20 11:09:09 +0100

    added solution

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 def main():
     total_points = 0
-    correct_answers = input('Wie viele richtige Antworten? ')
-    incorrect_answers = input('Wie viele falsche Antworten? ')
 
+    # Input values need to be converted to integers
+    correct_answers = int(input('Wie viele richtige Antworten? '))
+    incorrect_answers = int(input('Wie viele falsche Antworten? '))
+
+    # Correct logic for total points calculation
     total_points = correct_answers * 10 - incorrect_answers * 5
 
-    if total_points > 100
+    # Syntax error fixed: Missing colon after `if` statement
+    if total_points > 100:
         total_points = 100
+    # Syntax error fixed: Correct elif structure
     elif total_points < 0:
         total_points = 0
 
-    print('Endpunktzahl: ' + total_points)
+    # Fixed concatenation by using f-strings
+    print(f'Endpunktzahl: {total_points}')
 
 
-if __name__ = '__main__':
+# Syntax error fixed: `=` replaced with `==`
+if __name__ == '__main__':
     main()
