9b483769ac82816b2fdf54e8dafe4ef5070c3900
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-11-20 11:05:36 +0100

    init

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 14.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+def main():
+    total_points = 0
+    correct_answers = input('Wie viele richtige Antworten? ')
+    incorrect_answers = input('Wie viele falsche Antworten? ')
+
+    total_points = correct_answers * 10 - incorrect_answers * 5
+
+    if total_points > 100
+        total_points = 100
+    elif total_points < 0:
+        total_points = 0
+
+    print('Endpunktzahl: ' + total_points)
 
 
-if __name__ == '__main__':
-    calculate()
+if __name__ = '__main__':
+    main()
